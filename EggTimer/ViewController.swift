9ec56b99669a234557e6d13f9abf929458bbb13b
John Brennan <jbrennan@fluentstream.com>
2020-10-27 20:38:55 -0600

    setup alarm

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import Foundation
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var ChangeTitle: UILabel!
+    var player: AVAudioPlayer?
 
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    let alarmSound = URL(fileURLWithPath: Bundle.main.path(forResource: "alarm_sound", ofType: "mp3")!)
+
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle! //Soft, Medium, Hard
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        ChangeTitle.text = hardness
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+            if secondsPassed ==  totalTime {
+                ChangeTitle.text = "DONE!"
+                try! player = AVAudioPlayer(contentsOf: alarmSound)
+                player!.prepareToPlay()
+                player!.play()
+            }
+        }
+    }
 
 }
